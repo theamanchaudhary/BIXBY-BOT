cf84f2e004b178f5a63a5d2c6b73d598475f561c
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-03-18 07:46:58 +0530

    update database/users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -144,5 +144,18 @@ class Database:
     async def get_db_size(self):
         return (await self.db.command("dbstats"))['dataSize']
 
+    async def set_thumbnail(self, id, file_id):
+        await self.col.update_one({'_id': int(id)}, {'$set': {'file_id': file_id}})
 
+    async def get_thumbnail(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('file_id', None)
+
+    async def set_caption(self, id, caption):
+        await self.col.update_one({'_id': int(id)}, {'$set': {'caption': caption}})
+
+    async def get_caption(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('caption', None)
+        
 db = Database(DATABASE_URI, DATABASE_NAME)
