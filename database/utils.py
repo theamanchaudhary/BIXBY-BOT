d9f98dc2ee0b884d11e838b5945ca40be03856e1
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-03-18 08:47:51 +0530

    update database/utils.py

--- database/utils.py
+++ database/utils.py
@@ -1,6 +1,6 @@
 import math
 import time 
-from helper.txt import mr
+from Script import script
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram import enums
 
@@ -23,7 +23,7 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
             ''.join(["█" for i in range(math.floor(percentage / 5))]),
             ''.join(["░" for i in range(20 - math.floor(percentage / 5))]))
             
-        tmp = progress + mr.PROGRESS_BAR.format( 
+        tmp = progress + script.PROGRESS_BAR.format( 
             round(percentage, 2),
             humanbytes(current),
             humanbytes(total),
